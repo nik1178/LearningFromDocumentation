eb1139fcd21d3c96a10b81ded36bc0b02b27fa91
nik1178 <69012060+nik1178@users.noreply.github.com>
2021-09-15 19:46:00 +0200

    Add files via upload

---
+++ Decode.java
@@ -0,0 +1,7 @@
+public class Decode {
+    public static void main(String[] args) {
+        String str = "sjdasoij";
+        //Integer x = decode(str);
+        System.out.printf("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s", str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str, str);
+    }
+}
